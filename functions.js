935957397ca3ba6cdb884a6ab450c8751d8fc1c6
Linx Latham <linxlatham@gmail.com>
2017-09-20 20:31:13 -0700

    form update

--- functions.js
+++ functions.js
@@ -36,9 +36,11 @@ $(document).ready(function(){
     function yScroll(){
         nav = document.getElementById('nav-outer');
         logo = document.getElementById('logo');
-        flowers = document.getElementById('flowers');
         shine = document.getElementById('shine');
         body = document.getElementById('1');
+        registry = document.getElementById('5');
+        regContain = document.getElementById('reg')
+        regTop = registry.offsetTop - 150;
 
         yPos = window.pageYOffset;
 
@@ -47,75 +49,21 @@ $(document).ready(function(){
             logo.style.width = "30px";
             logo.style.marginTop = "11px";
             logo.style.marginBottom = "-12px";
-            flowers.style.top = "44%";
-            // shine.syle.top = "6px";
-            // shine.style.left = "49%";
             body.style.opacity = "0";
         } else {
             nav.style.height = "100px";
             logo.style.width = "50px";
             logo.style.marginTop = "20px";
             logo.style.marginBottom = "-26px";
-            flowers.style.top = "58%";
-            // shine.syle.top = "11px";
-            // shine.style.left = "48.7%";
             body.style.opacity = "1";
         }
-    }
 
- 			var app = new Vue({
-		  el: '#grid',
-		  data: {
-		    photos: [
-		    	'images/engagement4.jpg',
-		    	'images/engagement10.jpg',
-		    	'images/engagement5.jpg',
-		    	'images/engagement16.jpg',
-		    	'images/engagement12.jpg',
-		    	'images/engagement6.jpg',
-		    	'images/engagement13.jpg',
-		    	'images/engagement8.jpg',
-		    	'images/engagement9.jpg',
-		    	'images/engagement10.jpg',
-		    	'images/engagement11.jpg',
-		    	'images/engagement12.jpg',
-		    	'images/engagement13.jpg',
-		    	'images/engagement14.jpg',
-		    	'images/engagement15.jpg',
-		    	'images/engagement16.jpg',
-		    	'images/engagement17.jpg',
-		    	'images/engagement18.jpg',
-				'images/engagement19.jpg',
-		    	'images/engagement20.jpg',
-		    	'images/engagement21.jpg',
-		    	'images/engagement22.jpg',
-		    	'images/engagement23.jpg',
-		    	'images/engagement24.jpg',
-		    	'images/engagement25.jpg',
-		    	'images/engagement26.jpg',
-		    	'images/engagement27.jpg'
-		    ],
-		    current: {
-		    	page: 1,
-		    	beg: 0,
-		    	end: 8
-		    },
-		    modalOpen: false,
-		    selected: ''
-		  },
-		  methods: {
-		  	changePage: function(page, beg, end) {
-		  		this.current = {page: page, beg: beg, end: end};
-		  	},
-		  	openModal: function(img) {
-		  		this.selected = img;
-		  		this.modalOpen = true;
-		  	},
-		  	closeModal: function() { 
-		  		this.modalOpen = false;
-		  	}
-		  }
-		})
+        if (yPos > regTop) {
+        	regContain.style.opacity = 1
+        } else {
+        	regContain.style.opacity= 0
+        }
+    }
 
 	$('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -142,6 +90,16 @@ $(document).ready(function(){
       }
     }
   });
+
+  var guestCount = 1;
+  $('#add-guest').click(function(){
+  	
+  	let template = 	'<div class="col-xs-8"><input type="text" name="guest-' + guestCount + '" placeholder="name" /></div><div class="col-xs-4 select-holder"><select name="food-' + guestCount + '" class="select"><option value="ranch">From the Ranch</option><option value="sea">From the Sea</option><option value="farm">From the Farm</option><option value="children">Children</option></select><div class="select-arrow"></div></div>'
+  	
+  	$('#guests').append(template);
+
+  	guestCount += 1;
+  })
    
     countdown();   
     window.addEventListener("scroll", yScroll);
